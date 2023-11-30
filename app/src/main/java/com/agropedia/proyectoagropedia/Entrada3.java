26691db0f9ba89f6eb97e073cce65bdd3251ecf6
JhonAndresX <127310734+JhonAndresX@users.noreply.github.com>
2023-11-29 20:38:03 -0500

    Pruebaaaasadasdsa

---
+++ app/src/main/java/com/agropedia/proyectoagropedia/Entrada3.java
@@ -0,0 +1,14 @@
+package com.agropedia.proyectoagropedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Entrada3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada3);
+    }
+}
